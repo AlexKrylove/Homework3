569b3450a678180bef1061ba40ae76b1e1f1b0e4
AlexKrylov <krylofffff93@gmail.com>
2022-10-19 20:35:39 +0300

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Task1");
+        byte a = 15;
+        short b = 1115;
+        int c = 2204115;
+        long d = 1241354314L;
+        System.out.println("Значение переменной a с типом byte равно " + a);
+        System.out.println("Значение переменной b с типом short равно " + b);
+        System.out.println("Значение переменной c с типом int равно " + c);
+        System.out.println("Значение переменной d с типом long равно " + d);
     }
 }
